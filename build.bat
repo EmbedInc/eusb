4398b5b7dad9a2abd22f31c7346e02b2ecb344e0
Olin Lathrop <OlinLathrop@users.noreply.github.com>
2019-03-16 17:06:24 -0400

    EUSB 35.  UART is command source, not data port.
    Also updated the build scripts to the latest convention.

--- build.bat
+++ build.bat
@@ -3,43 +3,4 @@ rem
 rem   Build the firmware from this source directory.
 rem
 setlocal
-call build_fwinit
-
-call src_aspic %srcdir% %buildname%_ad
-call src_aspic %srcdir% %buildname%_adda
-call src_aspic %srcdir% %buildname%_cmd
-call src_aspic %srcdir% %buildname%_core12
-call src_aspic %srcdir% %buildname%_f16
-call src_aspic %srcdir% %buildname%_f16b
-call src_aspic %srcdir% %buildname%_f16e
-call src_aspic %srcdir% %buildname%_f18
-call src_aspic %srcdir% %buildname%_f30
-call src_aspic %srcdir% %buildname%_init
-call src_aspic %srcdir% %buildname%_intr
-call src_aspic %srcdir% %buildname%_led
-call src_aspic %srcdir% %buildname%_main
-call src_aspic %srcdir% %buildname%_mulu
-call src_aspic %srcdir% %buildname%_name
-call src_aspic %srcdir% %buildname%_op
-call src_aspic %srcdir% %buildname%_port
-call src_aspic %srcdir% %buildname%_prom
-call src_aspic %srcdir% %buildname%_regs
-call src_aspic %srcdir% %buildname%_sert
-call src_aspic %srcdir% %buildname%_stack
-call src_aspic %srcdir% %buildname%_task
-call src_aspic %srcdir% %buildname%_uart
-call src_aspic %srcdir% %buildname%_usb
-call src_aspic %srcdir% %buildname%_usb0
-call src_aspic %srcdir% %buildname%_vdd
-call src_aspic %srcdir% %buildname%_vpp
-call src_aspic %srcdir% %buildname%_wait
-
-call src_libpic %srcdir% %buildname%
-
-call src_aspic %srcdir% %buildname%_strt
-call src_expic %srcdir% %buildname%
-
-rem   Do SRC_GET on files just so that promotion is performed when enabled.
-rem
-src_get %srcdir% doc.txt
-src_get %srcdir% build.bat
+call build_fw
--- build_vars.bat
+++ build_vars.bat
@@ -1,10 +1,11 @@
 @echo off
 rem
-rem   Define the shell variables for running builds from this source library.
+rem   Define the variables for running builds from this source library.
 rem
 set srcdir=picprg
 set buildname=eusb
-call treename_var "(cog)source/%srcdir%/%buildname%" sourcedir
+call treename_var "(cog)source/picprg/eusb" sourcedir
 set fwname=eusb
 set pictype=18F2550
 set picclass=PIC
+set t_parms=
